dbd7609d9f73c40c970e1ecfaa5d8fb5966f60c0
NotArthu <71177564+ToprakDeveloper@users.noreply.github.com>
2023-02-12 21:07:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+print("------------Ehliyet Yaş Kontrolü------------")
+yas = int(input("Lütfen Yaşınızı Giriniz: "))
+if yas >= 18:
+    print("Yaşınız Ehliyet Almaya Uygun")
+else:
+    print("Yaşın Ehliyet Almaya Uygun Değil")
